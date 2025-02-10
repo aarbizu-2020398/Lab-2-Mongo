060ae685f73c1ee1123f5f61489e2cd88a0bfccf
Informatica <informatica@alumno.local>
2025-02-10 11:08:01 -0600

    Se terminaron los controladores, modelos y se hiceorn modificaciones

--- src/middlewares/validar-jwt.js
+++ src/middlewares/validar-jwt.js
@@ -1,37 +1,35 @@
 import jwt from 'jsonwebtoken';
-
+ 
 import Usuario from '../users/user.model.js';
-
+ 
 export const validarJWT = async (req, res, next) => {
-
+ 
     const token = req.header("x-token");
-
+ 
     if(!token){
         return res.status(401).json({
             msg: "No hay token en la peticion"
         })
     }
-
     try {
-        
         const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
-
+ 
         const usuario = await Usuario.findById(uid);
-
+ 
         if(!usuario){
             return res.status(401).json({
-                msg: 'Usuario no existe en la base de datos'
+                msg: "Usuario no existe en la base de datos"
             })
         }
-
+ 
         if(!usuario.estado){
             return res.status(401).json({
-                msg: 'Token no valido - usuarios con estado: false'
+                msg: "Token no valido - Usuario con estado: false"
             })
         }
-
+ 
         req.usuario = usuario;
-
+ 
         next();
     } catch (e) {
         console.log(e);
--- src/pet/pet.routes.js
+++ src/pet/pet.routes.js
@@ -1,21 +1,45 @@
-import {Router} from "express";
+import { Router } from "express";
 import { check } from "express-validator";
-import { savePet } from "./pet.controller.js";
-import { validarCampos } from "../middlewares/validar-campos.js";
-import{validarJWT} from '../middlewares/validar-jwt.js'
-
+import { SavePet, getPets, searchPets, deletePet } from "./pet.controller.js";
+import {validarCampos} from "../middlewares/validar-campos.js"
+import {validarJWT } from "../middlewares/validar-jwt.js"
+ 
 const router = Router();
-
+ 
 router.post(
     "/",
-
     [
-
         validarJWT,
-        check('email', 'Este no es un email valido').not().isEmpty(),
+        check("email","Este no es un correro valido").not().isEmpty(),
         validarCampos
     ],
-    savePet
+    SavePet
 )
-
+ 
+router.get(
+    "/",
+    getPets
+)
+ 
+router.get(
+    "/:id",
+    [
+        validarJWT,
+        check("id","No es un ID valido").isMongoId(),
+        validarCampos
+    ],
+    searchPets
+)
+ 
+router.delete(
+    "/:id",
+    [
+        validarJWT,
+        check("id","No es un ID valido").isMongoId(),
+        validarCampos
+    ],
+    deletePet
+ 
+)
+ 
 export default router;
